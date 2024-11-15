dc76b3f1ea79ca8d824be6e413c0f0cee73c6ae7
Mxolisi Hlakanyana <mxolisihlakanyana90@gmail.com>
2024-11-15 14:41:37 +0200

    my first commit

---
+++ my_list.py
@@ -0,0 +1,19 @@
+my_list = []
+my_list.append("10")
+my_list.append("20")
+my_list.append("30")
+my_list.append("40")
+#print(my_list)
+
+my_list.insert(1, "15")
+#print(my_list)
+my_list.extend(["50", "60", "70!"])
+#print(my_list)
+my_list.pop(1)
+#print(my_list)
+my_list.sort()
+#print(my_list)
+my_list = ["10" ,"20", "30", "40", "50", "60", "70"]
+index = my_list.index("30")
+print("The index of 30:", index)
+
